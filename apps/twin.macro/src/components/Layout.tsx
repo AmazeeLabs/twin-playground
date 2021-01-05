44333bdb48624966f22d560c269c315a642dafbc
Marco Tisi <marco.tisi@gmail.com>
2021-01-05 17:19:29 +0100

    feat: add global styles example

--- apps/twin.macro/src/components/Layout.tsx
+++ apps/twin.macro/src/components/Layout.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { GlobalStyles } from 'twin.macro';
+import { theme } from 'twin.macro';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle({
+  body: {
+    '--from-color': theme`colors.blue.500`,
+    '--to-color': theme`colors.white`,
+    backgroundImage: 'linear-gradient(var(--from-color), var(--to-color))',
+    fontFamily: 'sans-serif',
+  },
+});
 
 export const Layout: React.FC = ({ children, ...rest }) => (
   <div {...rest}>
-    <GlobalStyles />
+    <GlobalStyle />
     {children}
   </div>
 );
