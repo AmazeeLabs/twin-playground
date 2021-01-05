8193588b0f347694b2055f2c89516ee2ede9e0d9
Marco Tisi <marco.tisi@gmail.com>
2021-01-05 13:35:08 +0100

    feat: setup regular tailwind app

---
+++ apps/tailwind/gatsby-config.ts
@@ -0,0 +1,13 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+export const siteMetadata = {
+  title: 'Regular Tailwindcss',
+  description: 'Regular Tailwindcss example',
+  author: '@amazeelabs',
+};
+
+export const plugins = ['gatsby-plugin-postcss'];
